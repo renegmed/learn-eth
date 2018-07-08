e4d9983dfacea6fa5d3c2e02c255c03ecbd9d74e
rene <renegmed@yahoo.com>
2018-07-07 20:20:31 -0400

    adding frontend files to Github pages

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -1,6 +1,6 @@
 rsync -r src/ docs/
-rysnc build/contracts/ChainList.json docs/
-rysnc build/contracts/Ownable.json docs/
+rsync build/contracts/ChainList.json docs/
+rsync build/contracts/Ownable.json docs/
 git add .
 git commit -m "adding frontend files to Github pages"
 git push
